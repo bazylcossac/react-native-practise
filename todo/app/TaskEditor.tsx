98b4362557116596533e3c896265c4cc7e01362e
bazylcossac <bazyl.cossac@gmail.com>
2025-06-11 20:29:30 +0200

    add safeareaview

--- todo/app/TaskEditor.tsx
+++ todo/app/TaskEditor.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 
 export default function Edit() {
   return (
-    <View>
+    <SafeAreaView style={{flex: 1}}>
       <Text>edit page</Text>
-    </View>
+    </SafeAreaView>
   )
 }
--- todo/app/_layout.tsx
+++ todo/app/_layout.tsx
@@ -2,11 +2,13 @@ import {Tabs} from "expo-router"
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import AntDesign from '@expo/vector-icons/AntDesign';
 
+
+
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{tabBarActiveTintColor: "teal"}}>
-      <Tabs.Screen name="index" options={{tabBarIcon: ({color}) => <FontAwesome6 name="boxes-stacked" size={24} color={color} />}}/>
-      <Tabs.Screen name="edit" options={{tabBarIcon: ({color}) => <AntDesign name="edit" size={24} color={color} />}}/>
+    <Tabs screenOptions={{tabBarActiveTintColor: "teal", headerShown: false}} backBehavior="order" >
+      <Tabs.Screen name="TaskList"  options={{tabBarIcon: ({color}) => <FontAwesome6 name="boxes-stacked" size={24} color={color} />, tabBarLabel: "Main", tabBarShowLabel: false, headerTitle: () => null}}/>
+      <Tabs.Screen name="TaskEditor" options={{tabBarIcon: ({color}) => <AntDesign name="edit" size={24} color={color} />, tabBarLabel: "Edit", tabBarShowLabel: false, tabBarBadge: 22, tabBarBadgeStyle: {fontSize: 10}, headerTitle: () => null, popToTopOnBlur: true}}/>
     </Tabs>
   )
 }
